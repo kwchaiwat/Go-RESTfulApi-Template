27420e3ab2d3ecbe0d055531d00583ee6093e193
Chaiwat Kaewmukdasawan <67299853+kwchaiwat@users.noreply.github.com>
2021-07-29 14:55:51 +0700

    initConfig

---
+++ logs/logs.go
@@ -0,0 +1,32 @@
+package logs
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func init() {
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	var err error
+	log, err = config.Build()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+
+func Debug(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
-	"go-restful-api-template/databases"
 	"go-restful-api-template/handler"
 	"go-restful-api-template/repositories"
 	"go-restful-api-template/services"
+	"log"
 	"net/http"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
 )
 
 func main() {
-
-	// InitConfig
+	initConfig()
 	initTimeZone()
 	db := initDatabase()
 
@@ -32,7 +32,8 @@ func main() {
 	router.HandleFunc("/banks/{bankId:[0-9]+}", bankHandler.GetBank).Methods(http.MethodGet)
 
 	// ListenAndServe PORT 8000
-	http.ListenAndServe(":8000", router)
+	log.Printf("Banking service started at port: %v", viper.GetInt("app.port"))
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
 }
 
 func initTimeZone() {
@@ -44,7 +45,14 @@ func initTimeZone() {
 }
 
 func initDatabase() *sqlx.DB {
-	db, err := sqlx.Open("mysql", databases.DbURL(databases.BuildDBConfig()))
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
 	if err != nil {
 		fmt.Println("statuse: ", err)
 	}
@@ -52,4 +60,15 @@ func initDatabase() *sqlx.DB {
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
 	return db
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }

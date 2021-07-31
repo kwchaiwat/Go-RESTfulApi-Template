e81c1350edcbbc242954575ab83d9a2efbd53d39
Chaiwat Kaewmukdasawan <67299853+kwchaiwat@users.noreply.github.com>
2021-07-31 20:43:39 +0700

    refactory

--- repository/customer_repo.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import model "go-restful-api-template/models"
-
-type CustomerRepository interface {
-	GetAll() ([]model.Customer, error)
-	GetById(int) (*model.Customer, error)
-}
--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,11 @@ package repository
 
 import model "go-restful-api-template/models"
 
+type CustomerRepository interface {
+	GetAll() ([]model.Customer, error)
+	GetById(int) (*model.Customer, error)
+}
+
 type AccountRepository interface {
 	Create(model.Account) (*model.Account, error)
 	GetAll(int) ([]model.Account, error)
